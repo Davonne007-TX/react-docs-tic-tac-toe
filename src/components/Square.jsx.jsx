1607beeff1c5698e8e60b80d3c5a21f5f98ac0e9
Vonney <davonnejv@gmail.com>
2024-03-11 17:41:44 -0500

    adding comments, while reading the docs

--- src/components/Square.jsx.jsx
+++ src/components/Square.jsx.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 export default function Square() {
-  const [value, setValue] = useState(null);
+  const [value, setValue] = useState(null); //each square has its own state
 
   const handleClick = () => {
     setValue("X");
